e8a1fe45adf5055dc5df23ab8fa1db294f38e667
Mohammed Abdulhady <m.abdulhady00@gmail.com>
2021-05-02 19:21:28 +0200

    Queue Class cleanup

--- Mars Exploration/DataStructures/Queue.h
+++ Mars Exploration/DataStructures/Queue.h
@@ -10,77 +10,99 @@ private:
 	// and a tail pointer for the back of the queue.
 	node<T>* backPtr;
 	node<T>* frontPtr;
-	clearAll()
-	{
-		node<T> Temp=frontPtr;
-		while (frontPtr)
-		{
-			frontPtr = frontPtr->next;
-			delete Temp;
-			Temp = frontPtr;
-		}
-	}
+	void clearAll();
 public:
-	Queue():backPtr(nullptr),frontPtr(nullptr)
-	{
-	}
-	Queue(const Queue& aQueue)
-	{
-		node<T> Ptr = aQueue.frontPtr;
-		while (Ptr)
-		{
-			enqueue(Ptr->val);
-			Ptr = Ptr->next;
-		}
-	}
-	~Queue()
-	{
-		clearAll();
-	}
-	bool isEmpty() const
-	{
-		return (backPtr!=frontPtr);
-	}
-	bool enqueue(const T & newEntry) 
-	{
-		node<T>* newNodePtr = new node<T>(newEntry);
-		// Insert the new node
-		if (isEmpty())
-			frontPtr = newNodePtr; // The queue was empty
-		else
-			backPtr->next=newNodePtr; // The queue was not empty
-		backPtr = newNodePtr; // New node is at back
-		return true;
-	} // end enqueue
-	bool dequeue(T & newEntry) 
-	{
-		bool result = false;
-		if (!isEmpty())
-		{
-			newEntry = frontPtr->val;
-			// Queue is not empty; remove front
-			node<T>* nodeToDeletePtr = frontPtr;
-			if (frontPtr == backPtr)
-			{ // Special case: one node in queue
-				frontPtr = nullptr;
-				backPtr = nullptr;
-			}
-			else
-				frontPtr = frontPtr->next;
-			// Return deleted node to system
-			nodeToDeletePtr->next=nullptr;
-			delete nodeToDeletePtr;
-			nodeToDeletePtr = nullptr;
-			result = true;
-		} // end if
-		return result;
-	} // end dequeue
-	bool peekFront(T & value)
-	{
-		if (isEmpty())
-			return false;
-		value = frontPtr->val;
-		return true;
-	}
+	Queue();
+	Queue(const Queue& aQueue);
+
+	~Queue();
+
+	bool isEmpty() const;
+	bool enqueue(const T& newEntry); // end enqueue
+	bool dequeue(T& newEntry); // end dequeue
+	bool peekFront(T& value) const;
 };
 
+template<typename T>
+inline void Queue<T>::clearAll()
+{
+	node<T>* Temp = frontPtr;
+	while (frontPtr) {
+		frontPtr = frontPtr->next;
+		delete Temp;
+		Temp = frontPtr;
+	}
+}
+
+template<typename T>
+inline Queue<T>::Queue() :
+	backPtr(nullptr), 
+	frontPtr(nullptr)
+{}
+
+template<typename T>
+inline Queue<T>::Queue(const Queue & aQueue)
+{
+	node<T>* Ptr = aQueue.frontPtr;
+	while (Ptr) {
+		enqueue(Ptr->val);
+		Ptr = Ptr->next;
+	}
+}
+
+template<typename T>
+inline Queue<T>::~Queue()
+{
+	clearAll();
+}
+
+template<typename T>
+inline bool Queue<T>::isEmpty() const
+{
+	return frontPtr == nullptr;
+}
+
+template<typename T>
+inline bool Queue<T>::enqueue(const T& newEntry)
+{
+	node<T>* newNodePtr = new node<T>(newEntry);
+	// Insert the new node
+	if (isEmpty())
+		frontPtr = newNodePtr; // The queue was empty
+	else
+		backPtr->next = newNodePtr; // The queue was not empty
+	backPtr = newNodePtr; // New node is at back
+	return true;
+} // end enqueue
+
+template<typename T>
+inline bool Queue<T>::dequeue(T& newEntry)
+{
+	bool result = false;
+	if (!isEmpty()) {
+		newEntry = frontPtr->val;
+		// Queue is not empty; remove front
+		node<T>* nodeToDeletePtr = frontPtr;
+		if (frontPtr == backPtr) { // Special case: one node in queue
+			frontPtr = nullptr;
+			backPtr = nullptr;
+		}
+		else
+			frontPtr = frontPtr->next;
+		// Return deleted node to system
+		nodeToDeletePtr->next = nullptr;
+		delete nodeToDeletePtr;
+		nodeToDeletePtr = nullptr;
+		result = true;
+	} // end if
+	return result;
+} // end dequeue
+
+template<typename T>
+inline bool Queue<T>::peekFront(T& value) const
+{
+	if (isEmpty())
+		return false;
+	value = frontPtr->val;
+	return true;
+}
